9e33a74505cf65aac7dfe8c5647fc2e65978e6b5
Aishani Sahoo <asahoo4@illinois.edu>
2023-12-05 15:31:46 -0600

    Added a Procfile.

--- backend stuff/routes/leagues.js
+++ backend stuff/routes/leagues.js
@@ -1,49 +1,50 @@
-var teamdb = require("../Models/Teams");
-var leaguedb = require("../Models/Leagues");
-var express = require("express");
-var api = express.Router();
-var mongoose = require("mongoose");
+const express = require("express");
+const api = express.Router();
+const mongoose = require("mongoose");
+const leaguedb = require("../Models/Leagues");
 
-api.get("/:id", (req, res)=> {
-    leaguedb.findOne({_id: req.params.id})
-        .then(function (info) {
-            if (info) {
-                res.status(200).send({message: "OK", data: info});
-            } else {
-                res.status(404).send({message: "404 League could not be found", data: {}});
-            }
-        })
-        .catch(function (err) {
-            res.status(404).send({message: "404 League could not be found", data: {}});
-        })
+api.get("/:id", async (req, res) => {
+    try {
+        const info = await leaguedb.findOne({ _id: req.params.id });
+        if (info) {
+            res.status(200).json({ message: "OK", data: info });
+        } else {
+            res.status(404).json({ message: "404 League could not be found", data: {} });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", data: {} });
+    }
 });
 
-api.post("/", async (req, res)=> {
-    if (req.body.name && req.body.join_code) {
-        var exsistingLeague = await leaguedb.findOne({name: req.body.name})
-        if (exsistingLeague != null) {
-            res.status(400).send({message: "This league already exsist", data: exsistingLeague});
+api.post("/", async (req, res) => {
+    try {
+        if (req.body.name && req.body.join_code) {
+            const existingLeague = await leaguedb.findOne({ name: req.body.name });
+            if (existingLeague) {
+                res.status(400).json({ message: "This league already exists", data: existingLeague });
+            } else {
+                const newLeague = await leaguedb.create({ _id: new mongoose.Types.ObjectId(), ...req.body });
+                res.status(201).json({ message: "League Created", data: newLeague });
+            }
         } else {
-            var newLeague = await leaguedb.create({_id: new mongoose.Types.ObjectId, ...req.body})
-                .catch((err) => {res.status(500).send({message: "Server Failure occured1", data:{}});});
-                res.status(202).send({message: "League Created", data: newLeague});
-        } 
-    } else {
-        res.status(400).send({message: "you are missing a name or join code", data:{}});
+            res.status(400).json({ message: "You are missing a name or join code", data: {} });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", data: {} });
     }
 });
 
 api.put("/:id", async (req, res) => {
-    var exsistingLeague = await leaguedb.findOne({_id: req.params.id})
-        .catch((err) => {
-            res.status(500).send({message: "Server Failure occured", data:{}});
-        });
-    if (exsistingLeague) {
-        var updatedaLeague = await leaguedb.findOneAndUpdate({_id : req.params.id}, {...req.body})
-            .catch((err) => {res.status(500).send({message: "Server Failure occured", data:{}});});
-        res.status(202).send({message: "Updated League", data: updatedaLeague});
-    } else {
-        res.status(400).send({message: "This league does't exsist", data: {}});
+    try {
+        const existingLeague = await leaguedb.findOne({ _id: req.params.id });
+        if (existingLeague) {
+            const updatedLeague = await leaguedb.findOneAndUpdate({ _id: req.params.id }, { ...req.body });
+            res.status(200).json({ message: "Updated League", data: updatedLeague });
+        } else {
+            res.status(404).json({ message: "This league doesn't exist", data: {} });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", data: {} });
     }
 });
 
