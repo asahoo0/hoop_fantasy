d87cfb074320c3d5f3883bc76188ebc028d7b1b0
Aishani Sahoo <asahoo4@illinois.edu>
2023-12-06 18:48:20 -0600

    changes

--- src/config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:4000' : 'https://sleepy-inlet-41175-938382755436.herokuapp.com';
-
-export const API_BASE_URL = baseUrl;
