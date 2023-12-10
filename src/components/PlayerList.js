7b9a4ab7ec1914f46c5e9dfee94b0a9f5b784425
Andrew Elysee <32715121+aceprime80@users.noreply.github.com>
2023-12-10 14:30:29 -0600

    beginning of player search functionality

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -3,40 +3,70 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import { useNavigate } from 'react-router-dom';
+import { filterCurrent } from './id';
+import "./PlayerList.scss"
 
 const PlayerList = () => {
   const [players, setPlayers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const navigate = useNavigate();
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get(`https://www.balldontlie.io/api/v1/players?page=${currentPage}&per_page=10`);
-        setPlayers(response.data.data);
-      } catch (error) {
-        console.error('Error fetching players:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const [currentPage, setCurrentPage] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    fetchPlayers();
-  }, [currentPage]);
+  
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   const fetchPlayers = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await axios.get(`https://www.balldontlie.io/api/v1/players?page=${currentPage}&per_page=10`);
+  //       // const response = await axios.get(`https://www.balldontlie.io/api/v1/players?search=`);
+  //       // const response1 = await axios.get('https://www.balldontlie.io/api/v1/season_averages?season=2023')
+  //       // console.log(responsetest)
+  //       // console.log(response1)
+  //       setPlayers(response.data.data);
+  //     } catch (error) {
+  //       console.error('Error fetching players:', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchPlayers();
+  // }, [currentPage]);
 
   const formatPlayerName = (player) => {
     return `${player.id}. ${player.first_name} ${player.last_name}`;
   };
-
+  const handleSearch = async () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${searchTerm}&per_page=100`)
+      .then((res)=>{
+        console.log(res.data.data)
+        setPlayers(filterCurrent(res.data.data))
+        setLoading(false);
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+  };
   return (
     <div>
       <NavBar />
       <div className='main_item'>
         <h1>Ball Don't Lie Player List</h1>
         <button className="standard_button" onClick = {() => navigate("/your-leagues")}>Back to Your Leagues</button>
+        <div>
+          <h1>Search For Players</h1>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
         {loading ? (
-          <p>Loading...</p>
+          <div></div>
+          // <p>Loading...</p>
         ) : (
           <>
             <ul>
